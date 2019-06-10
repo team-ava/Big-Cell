52f3bd21a4e3b94bb8899c1ad1f946e14b8885c5
Anderson Gonçalves <Andersontno@gmail.com>
2019-06-10 19:38:24 -0300

    Validate vendas completo

--- app/models/venda.rb
+++ app/models/venda.rb
@@ -1,4 +1,9 @@
 class Venda < ApplicationRecord
     has_one :cliente
     has_one :produto
+    
+    validates :clienteId, presence: { message: 'é um campo obrigatório' }
+    validates :produtoId, presence: { message: 'é um campo obrigatório' }
+    validates :valorServico, length: {maximum: 6, message: 'excede o valor padrão de serviços'}
+    validates_numericality_of :valorServico, message: 'deve ser um número' 
 end
