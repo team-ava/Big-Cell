a1faa203a8e6cf6ad5b67fa2472da0e53f2e6148
Anderson Gonçalves <Andersontno@gmail.com>
2019-06-11 00:37:23 -0300

    Validates Concluidos

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,4 +1,10 @@
 class Cliente < ApplicationRecord
-    validates :nome, presence: { message: 'é um campo obrigatório' }
-    validates :cpf, uniqueness: {menssage: 'campo já existente'}
+    validates :nome, presence: {message: 'é um campo obrigatório'}
+    
+    validates :cpf, presence: {message: 'é um campo obrigatório'}
+    validates :cpf, uniqueness: {message: 'campo já existente'}
+    validate  :cpf, length: {minimum: 11,maximum: 14}
+
+    validates :fone, presence: {message: 'é um campo obrigatório'}
+    validates :fone, length: {minimum: 11,maximum: 11}
 end
--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,3 +1,12 @@
 class Pedido < ApplicationRecord
     has_one :cliente
+
+    validates :clienteId, presence: { message: 'é um campo obrigatório' }
+
+    validates :nome, presence: { message: 'é um campo obrigatório' }
+
+    validates :descricao, presence: { message: 'é um campo obrigatório' }
+    
+    
+    
 end
